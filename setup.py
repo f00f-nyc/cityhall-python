a71a0c0aaf92bda6decc0b0191f0541cff4e9651
Alex Popa <alex.popa@gmail.com>
2015-10-02 15:14:06 -0400

    Update setup.py to try to get requirments recongized

--- setup.py
+++ setup.py
@@ -1,36 +1,24 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-import os
-
 try:
     from setuptools import setup
 except ImportError:
     from distutils.core import setup
 
 
-def read(fname):
-    return (open(os.path.join(os.path.dirname(__file__), fname), 'rb')
-            .read().decode('utf-8'))
-
-
-test_requirements = (
-    read('requirements.txt').splitlines()
-    + read('requirements_test.txt').splitlines()[1:]
-)
-
-
 setup(
     name = 'cityhall',
     packages = ['cityhall'], # this must be the same as the name above
-    version = '0.0.6',
+    version = '0.0.10',
     description = 'A library for accessing City Hall Setting Server',
+    license = 'AGPL',
     author = 'Alex Popa',
     author_email = 'alex.popa@digitalborderlands.com',
     url = 'https://github.com/f00f-nyc/cityhall-python',
     download_url = 'https://codeload.github.com/f00f-nyc/cityhall-python/legacy.tar.gz/v0.0.6',
-    install_requires=read('requirements.txt').splitlines(),
+    install_requires=['requests==2.7.0','six==1.9.0'],
     keywords = ['cityhall', 'enterprise settings', 'settings', 'settings server', 'cityhall', 'City Hall'],
     test_suite='test',
-    tests_require=test_requirements,
+    tests_require=['requests==2.7.0','six==1.9.0','mock==1.0.1'],
     classifiers = [],
 )
